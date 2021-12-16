27e9d7634597819621583be653af5ebb5a150516
Yana Yukhimuk <61915492+yanayukhimuk@users.noreply.github.com>
2021-12-16 13:29:21 +0300

    new

---
+++ Methods_and_Functions/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Methods_and_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char s;
+            uint times;
+            s = GetSymbol();
+            times = GetHowmanyTimesShowChar();
+            PrintString(s, times);
+
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("What number do you want to find?");
+            int value = int.Parse(Console.ReadLine());
+            Console.WriteLine("The index of " + value + " is: " + GetIndexOfElement(arr, value));
+        }
+
+        static char GetSymbol()
+        {
+            char symb;
+            Console.WriteLine("Enter your symbol or letter: ");
+            symb = char.Parse(Console.ReadLine());
+            return symb;
+        }
+
+        static uint GetHowmanyTimesShowChar()
+        {
+            uint times;
+            Console.WriteLine("Enter how many times you want to show the symbol: ");
+            times = uint.Parse(Console.ReadLine());
+            return times;
+            
+        }
+
+        static void PrintString(char str, uint t)
+        {
+            for (int i = 0; i < t; i++)
+            {
+                Console.Write(str);
+            }
+        }
+
+        static int GetIndexOfElement(int[] arr, int value)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ New_Net_Core/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace New_Net_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] myArray = { 2, 10, 5, 6, 77, 89 };
+            //Console.WriteLine(myArray[^1]);
+
+            //int[] myArray2 = myArray[1..4];
+
+            //for (int i = 0; i < myArray2.Length; i++)
+            //{
+            //    Console.WriteLine(myArray2[i]);
+            //}
+
+            //string str = "I am the best of the best";
+            //Console.WriteLine(str[^5..]);
+
+            int[,] my2DArray = new int[5, 10];
+                    
+            int height = my2DArray.GetLength(0);
+            int width = my2DArray.GetLength(1);
+
+            Random random = new Random();
+
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int k = 0; k < width; k++)
+                {
+                    my2DArray[i, k] = random.Next(100);
+                }
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int k = 0; k < width; k++)
+                {
+                    Console.Write(my2DArray[i, k] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int[][] unusualArray = new int[3][];
+            unusualArray[0] = new int[5];
+            unusualArray[1] = new int[4];
+            unusualArray[2] = new int[2];
+
+            for (int i = 0; i < unusualArray.Length; i++)
+            {
+                for (int k = 0; k < unusualArray[i].Length; k++)
+                {
+                    unusualArray[i][k] = random.Next(20);
+                }
+            }
+
+            for (int i = 0; i < unusualArray.Length; i++)
+            {
+                for (int k = 0; k < unusualArray[i].Length; k++)
+                {
+                    Console.Write(unusualArray[i][k] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(); 
+
+
+            int[,,] new3dArray = new int[4, 3, 5];
+            int volume = new3dArray.GetLength(0);
+            int height2 = new3dArray.GetLength(1);
+            int width2 = new3dArray.GetLength(1);
+
+            for (int i = 0; i < volume; i++)
+            {
+                for (int k = 0; k < height2; k++)
+                {
+                    for (int j = 0; j < width2; j++)
+                    {
+                        new3dArray[i, k, j] = random.Next(100);
+                    }
+                }
+            }
+
+            for (int i = 0; i < volume; i++)
+            {
+                Console.WriteLine("Page #" + (i+1));
+                for (int k = 0; k < height2; k++)
+                {
+                    for (int j = 0; j < width2; j++)
+                    {
+                        Console.Write (new3dArray[i, k, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("##################"); 
+            }
+
+
+        }
+    }
+}
